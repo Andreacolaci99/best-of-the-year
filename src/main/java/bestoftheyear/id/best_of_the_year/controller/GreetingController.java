14a41e7b10fbf4254fa33fd1dfe0a1d94f3871af
Andreacolaci99 <colaciandrea99@gmail.com>
2025-04-03 16:12:43 +0200

    feat : End Exercise

--- src/main/java/bestoftheyear/id/best_of_the_year/controller/GreetingController.java
+++ src/main/java/bestoftheyear/id/best_of_the_year/controller/GreetingController.java
@@ -18,7 +18,7 @@ import bestoftheyear.id.best_of_the_year.models.Songs;
 public class GreetingController {
 
     @GetMapping("/greeting")
-    public String greeting(@RequestParam(name = "name") String name, Model model) {
+    public String greeting(@RequestParam(name = "name", required=false, defaultValue = "creator") String name, Model model) {
         model.addAttribute("name", name);
         return "greeting";
     }
@@ -28,17 +28,21 @@ public class GreetingController {
         Movie m1 = new Movie(1, "Tre uomini e una gamba");
         Movie m2 = new Movie(2, "La leggenda di Al Jhon e Jack");
         Movie m3 = new Movie(3, "Chiedimi se sono felice");
+        Movie m4 = new Movie(4, "Tu la conosci Claudia ?");
+        Movie m5 = new Movie(5, "Odio l'estate");
 
         List<Movie> movie = new ArrayList<>();
 
         movie.add(m1);
         movie.add(m2);
         movie.add(m3);
+        movie.add(m4);
+        movie.add(m5);
 
         return movie;
     }
 
-    @GetMapping("/movie")
+    @GetMapping("/greeting/movie")
     public String BestMovies(Model model) {
         List<Movie> movie = getBestMovies();
         model.addAttribute("movie", movie);
@@ -46,19 +50,24 @@ public class GreetingController {
     }
 
     private List<Songs> getBestSongs() {
-        Songs s1 = new Songs(1, "Giudizi universali");
-        Songs s2 = new Songs(2, "Replay");
-        Songs s3 = new Songs(3, "Spaccacuore");
+
+        Songs s1 = new Songs(1, "Pasta Fasule");
+        Songs s2 = new Songs(2, "Giudizi universali");
+        Songs s3 = new Songs(3, "Replay");
+        Songs s4 = new Songs(4, "Spaccacuore");
+        Songs s5 = new Songs(5, "I'm your man");
 
         List<Songs> songs = new ArrayList<>();
         songs.add(s1);
         songs.add(s2);
         songs.add(s3);
+        songs.add(s4);
+        songs.add(s5);
 
         return songs;
     }
 
-    @GetMapping("/songs")
+    @GetMapping("/greeting/songs")
     public String BestSongs(Model model) {
         List<Songs> songs = getBestSongs();
         model.addAttribute("songs", songs);
